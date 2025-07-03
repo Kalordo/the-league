89177a8c45914f72343e88e2e3f20094bde8fcae
Paulbrg35 <bourgeon.gestion@gmail.com>
2025-07-03 08:44:50 +0000

    the-league

--- config/database.php
+++ config/database.php
@@ -4,5 +4,4 @@
     private $db_name = 'paulbourgeon_The_League';
     private $username = 'paulbourgeon';
     private $password = '1e737408cf748bbd6ec9667085ab9238';
-    public $connexion;
     }
