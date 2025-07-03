df2321c9948a40d9e6c50d786580a161b0c75b97
Paulbrg35 <bourgeon.gestion@gmail.com>
2025-07-03 08:41:52 +0000

    the-league

---
+++ config/database.php
@@ -0,0 +1,8 @@
+<?php
+    class Database {
+    private $host = 'db.3wa.io';
+    private $db_name = 'paulbourgeon_The_League';
+    private $username = 'paulbourgeon';
+    private $password = '1e737408cf748bbd6ec9667085ab9238';
+    public $connexion;
+    }
