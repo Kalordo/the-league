3cf238c6115b851043d06bd5261cb5d95cbf07b6
Kalordo <yohann.leperson@gmail.com>
2025-07-03 16:19:13 +0200

    Ajout des méthodes findAll et findOne dans GamesManager pour récupérer les jeux depuis la base de données

--- managers/GamesManager.php
+++ managers/GamesManager.php
@@ -5,5 +5,41 @@ class GamesManager extends AbstractManager {
         parent::__construct();        
     }
 
-    
+    public function findAll(): array {
+        $result = $this->db->query("SELECT * FROM games");
+        $gamesResult = $result->fetchAll(PDO::FETCH_ASSOC);
+
+        $games = [];
+
+        foreach ($gamesResult as $gameResult) {
+            $games[] = new Games(
+                (int) $gameResult['id'],
+                $gameResult['name'],
+                $gameResult['date'],
+                (int) $gameResult['team_1'],
+                (int) $gameResult['team_2'],
+                (int) $gameResult['winner']
+            );
+        }
+
+        return $games;
+    }
+
+    public function findOne(int $id): ?Games {
+        $stmt = $this->db->prepare("SELECT * FROM games WHERE id = :id");
+        $stmt->execute([':id' => $id]);
+        $gameResult = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($gameResult) {
+            return new Games(
+                (int) $gameResult['id'],
+                $gameResult['name'],
+                $gameResult['date'],
+                (int) $gameResult['team_1'],
+                (int) $gameResult['team_2'],
+                (int) $gameResult['winner']
+            );
+        }
+        return null;
+    }
 }
