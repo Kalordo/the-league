3e46ab0ba9703b4957b430c78f03c882f5136271
Kalordo <yohann.leperson@gmail.com>
2025-07-03 14:53:46 +0200

    ajout des méthodes findAll et findOne dans la classe PlayersManagers pour récupérer les joueurs depuis la base de données

--- managers/PlayersManagers.php
+++ managers/PlayersManagers.php
@@ -4,4 +4,40 @@ class PlayersManagers extends AbstractManager {
     public function __construct() {
         parent::__construct();
     }
+
+    public function findAll(): array {
+        $result = $this->db->query("SELECT * FROM players");
+        $playersResult = $result->fetchAll(PDO::FETCH_ASSOC);
+
+        $players = [];
+
+        foreach ($playersResult as $playerResult) {
+            $players[] = new Player(
+                (int) $playerResult['id'],
+                $playerResult['nickname'],
+                $playerResult['bio'],
+                $playerResult['portrait'],
+                $playerResult['team']
+            );
+        }
+
+        return $players;
+    }
+
+    public function findOne(int $id): ?Player {
+        $stmt = $this->db->prepare("SELECT * FROM players WHERE id = :id");
+        $stmt->execute([':id' => $id]);
+        $playerResult = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($playerResult) {
+            return new Player(
+                (int) $playerResult['id'],
+                $playerResult['nickname'],
+                $playerResult['bio'],
+                $playerResult['portrait'],
+                $playerResult['team']
+            );
+        }
+        return null;
+    }
 }
