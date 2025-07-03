488bd2ab8a4d1c956cd1373f24e6128f5499885e
nailuJ-dev <julian.buzare@gmail.com>
2025-07-03 11:26:06 +0000

    add autoload.php + 404.html.twig

---
+++ config/autoload.php
@@ -0,0 +1,26 @@
+<?php
+
+/* MODELS */
+require "models/Game.php";
+require "models/Media.php";
+require "models/Player.php";
+require "models/PlayerPerformance.php";
+require "models/Team.php";
+
+/* MANAGERS */
+require "managers/AbstractManager.php";
+require "managers/GamesManager.php";
+require "managers/MediaManager.php";
+require "managers/PerformanceManager.php";
+require "managers/PlayersManager.php";
+require "managers/TeamsManager.php";
+
+/* CONTROLLERS */
+require "controller/HomeController.php";
+require "controller/MatchesController.php";
+require "controller/PlayersController.php";
+require "controller/TeamsController.php";
+
+/* SERVICES */
+require "config/databse.php";
+require "config/Router.php";
